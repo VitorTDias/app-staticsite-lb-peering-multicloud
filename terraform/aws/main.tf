74b537fb7f128fba9aa6cf6c29484e745229958d
Vitor <vitortortodias@gmail.com>
2024-11-12 11:55:53 -0300

    verificando erro6.91

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -9,12 +9,12 @@ module "rede" {
 
 module "compute" {
     source     = "./modules/compute"
-    rede10_id    = "${modules.rede.vpc10_id}"
-    rede20_id    = "${modules.rede.vpc20_id}"
-    subnet_pub_cidr  = "${modules.rede.subnet_pub_id}"
-    subnet_pri_cidr  = "${modules.rede.subnet_pri_id}"
-    rede_pub_cidr  = "${var.vpc10_cidr}"
-    rede_pri_cidr  = "${var.vpc20_cidr}"
+    vpc10_id    = "${modules.rede.vpc10_id}"
+    vpc20_id    = "${modules.rede.vpc20_id}"
+    # subnet_pub_cidr  = "${modules.rede.subnet_pub_id}"
+    # subnet_pri_cidr  = "${modules.rede.subnet_pri_id}"
+    vpc10_cidr  = "${var.vpc10_cidr}"
+    vpc20_cidr  = "${var.vpc20_cidr}"
     ami        = "${var.ami}"
     depends_on = [modules.rede]
 }

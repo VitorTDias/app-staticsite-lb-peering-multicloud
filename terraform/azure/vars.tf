756991ea812e16168a247af4236fde4542a68dc6
Vitor <vitortortodias@gmail.com>
2024-11-12 12:41:01 -0300

    verificando erro9

--- terraform/azure/vars.tf
+++ terraform/azure/vars.tf
@@ -8,14 +8,24 @@ variable "location" {
     default = "brazilsouth"
 }
 
-variable "vnet_cidr" {
+variable "vnet10_cidr" {
     type    = string
     default = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
+variable "subnet_vnet10_cidr" {
     type    = string
     default = "10.0.1.0/24"
+}
+
+variable "vnet20_cidr" {
+    type    = string
+    default = "20.0.0.0/16"
+}
+
+variable "subnet_vnet20_cidr" {
+    type    = string
+    default = "20.0.1.0/24"
 }
 
 variable "fqdn" {

fa6e507a8ce1e41f099507e51ad28244e1a9efac
Vitor <vitortortodias@gmail.com>
2024-11-13 15:29:38 -0300

    ok

--- terraform/azure/modules/rede/output.tf
+++ terraform/azure/modules/rede/output.tf
@@ -9,3 +9,11 @@ output "subnet_vnet10b_id" {
 output "subnet_vnet20_id" {
     value = "${azurerm_subnet.subnet_vnet20.id}"
 }
+
+output "vnet10_id" {
+    value = "${azurerm_virtual_network.vnet10.id}"
+}
+
+output "vnet20_id" {
+    value = "${azurerm_virtual_network.vnet20.id}"
+}
